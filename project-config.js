4cf772a6ba577d591678a77f1320efe663e0b2b2
Karthik Naidu <knaidudintakurthi@deloitte.com>
2018-01-25 17:44:30 +0530

    Fixed components create path issue in generators

--- project-config.js
+++ project-config.js
@@ -1,8 +1,8 @@
 /*
-* Project Setting & Configuration
-* Compilation : Handlebars, Assembler, ReactJS, SCSS
-* Build Tool : Webpack v3
-*/
+ * Project Setting & Configuration
+ * Compilation : Handlebars, Assembler, ReactJS, SCSS
+ * Build Tool : Webpack v3
+ */
 
 // Application
 const APP_NAME = 'New Web Design';
@@ -17,7 +17,7 @@ const JS_LIB = `${SOURCE_ROOT_FOLDER}/lib`;
 
 // Handlebars
 const HANDLEBARS_DIR = `${SOURCE_ROOT_FOLDER}/hbs-app`;
-const HBS_FE_COMPONENTS = `${HANDLEBARS_DIR}/fe-components`;
+const HBS_FE_COMPONENTS = `${HANDLEBARS_DIR}/fe-components/`;
 const HBS_FE_LAYOUTS = `${HANDLEBARS_DIR}/layouts`;
 const HBS_FE_PAGES = `${HANDLEBARS_DIR}/pages`;
 
@@ -39,9 +39,9 @@ const OUTPUT_CSS_FOLDER = 'css';
 const OUTPUT_ASSETS_FOLDER = 'assets'; // Public Folder
 
 /*
-*    /site-name  => If site is hosted on Shared Machine
-*    /           => If site is hosted on Dedicated Machine
-*/
+ *    /site-name  => If site is hosted on Shared Machine
+ *    /           => If site is hosted on Dedicated Machine
+ */
 const APP_PUBLIC_PATH = 'xyz-webiste';
 
 // Webpack-Dev-Server
@@ -50,25 +50,25 @@ const DEV_SERVER_PORT = 6565;
 const API_BASE_URL = 'http://<HOST_NAME>:<PORT>'; // Without Ending '/'
 
 module.exports = {
-	APP_NAME,
-	APP_TITLE,
-	VERSION,
+    APP_NAME,
+    APP_TITLE,
+    VERSION,
 
-	SOURCE_ROOT_FOLDER,
-	JS_LIB,
-	HANDLEBARS_DIR,
-	HBS_FE_COMPONENTS,
-	HBS_FE_LAYOUTS,
-	HBS_FE_PAGES,
-	HBS_DEPENDENCY_POINT,
-	STYLESHEETS,
-	ASSETS_SRC,
-	WEB_ROOT,
-	OUTPUT_JS_FOLDER,
-	OUTPUT_CSS_FOLDER,
-	OUTPUT_ASSETS_FOLDER,
-	APP_PUBLIC_PATH,
-	DEV_SERVER_HOST,
-	DEV_SERVER_PORT,
-	API_BASE_URL
+    SOURCE_ROOT_FOLDER,
+    JS_LIB,
+    HANDLEBARS_DIR,
+    HBS_FE_COMPONENTS,
+    HBS_FE_LAYOUTS,
+    HBS_FE_PAGES,
+    HBS_DEPENDENCY_POINT,
+    STYLESHEETS,
+    ASSETS_SRC,
+    WEB_ROOT,
+    OUTPUT_JS_FOLDER,
+    OUTPUT_CSS_FOLDER,
+    OUTPUT_ASSETS_FOLDER,
+    APP_PUBLIC_PATH,
+    DEV_SERVER_HOST,
+    DEV_SERVER_PORT,
+    API_BASE_URL
 };
